0b75f3100693a5d9c3bab562de569f03f659323b
sasa1977 <sasa.juric@gmail.com>
2013-02-05 23:36:23 +0100

    package.exs

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(
+  name: "lambda", description: "Syntactic sugar for defining lambda functions with explicit scope",
+  version: "0.1", keywords: ["elixir", "lambda"], 
+  maintainers: [[name: "Sasa Juric", email: "sasa.juric@gmail.com"]],
+  repositories: [[github: "sasa1977/lambda"]]
+)
